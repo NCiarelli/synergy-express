0239e3a21fda078408f781c80a335c32323f6d56
Casey <clounsbur@gmail.com>
2019-12-06 15:58:41 -0500

    Change the api url to coming from the environment variables.

--- routes.js
+++ routes.js
@@ -2,11 +2,8 @@
 const express = require("express");
 const personalityInsights = require("./watson-api");
 
-
 // Setup a routing object for the server
 const routes = express.Router();
-
-
 
 // Quick response from server to show its alive and working
 routes.get("/", (req, res) => {
@@ -15,26 +12,25 @@ routes.get("/", (req, res) => {
 
 // Generate a new Employee Personaolity Profile
 routes.post("/profile", (req, res) => {
-
   // Setup get profile request for Watson
   let profileParams = {
     // Get the content from the request sent to the server.
     content: req.body,
-    contentType: 'application/json',
+    contentType: "application/json",
     consumptionPreferences: false,
-    rawScores: true,
+    rawScores: true
   };
   // Send get profile request to Watson
-  personalityInsights.profile(profileParams)
+  personalityInsights
+    .profile(profileParams)
     .then(profile => {
       // console.log(JSON.stringify(profile, null, 2));
       // Send the resulting profile back to the Angular App
       res.json(profile);
     })
     .catch(err => {
-      console.log('error:', err);
+      console.log("error:", err);
     });
-})
-
+});
 
 module.exports = routes;
--- watson-api.js
+++ watson-api.js
@@ -1,21 +1,21 @@
 // Import Watson SDK elements
-const PersonalityInsightsV3 = require('ibm-watson/personality-insights/v3');
-const { IamAuthenticator } = require('ibm-watson/auth');
+const PersonalityInsightsV3 = require("ibm-watson/personality-insights/v3");
+const { IamAuthenticator } = require("ibm-watson/auth");
 
 try {
   // When not running via Heroku, this will load the .env file.
-  require('dotenv').config();
+  require("dotenv").config();
 } catch (e) {
   // When running with Heroku, dotenv doesn't exist so problem NEED TO FIX
 }
 
 // Setup the Authentication
 const personalityInsights = new PersonalityInsightsV3({
-  version: '2017-10-13',
+  version: "2017-10-13",
   authenticator: new IamAuthenticator({
-    apikey: process.env.API_KEY,
+    apikey: process.env.API_KEY
   }),
-  url: 'https://gateway.watsonplatform.net/personality-insights/api',
+  url: process.env.API_URL
 });
 
 module.exports = personalityInsights;
