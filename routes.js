4453b720332ac4858e1e99ec993f8d4da3c6fb6b
Ncia89 <nicholas.ciarelli@gmail.com>
2019-12-11 10:48:33 -0500

    Made changes to allow using heroku.

--- routes.js
+++ routes.js
@@ -2,6 +2,9 @@
 const express = require("express");
 const personalityInsights = require("./watson-api");
 
+// Stuff to save profile to a file
+let fs = require('fs');
+
 // Setup a routing object for the server
 const routes = express.Router();
 
@@ -27,10 +30,22 @@ routes.post("/profile", (req, res) => {
       // console.log(JSON.stringify(profile, null, 2));
       // Send the resulting profile back to the Angular App
       res.json(profile);
+
+      // // Write the profile to a file REMOVE FOR PRODUCTION
+      // let jsonData = JSON.stringify(profile);
+      // fs.writeFile("profile_saves/lastProfile.json", jsonData, function (err) {
+      //   if (err) {
+      //     console.log(err);
+      //   }
+      // });
+      // console.log("Wrote profile to file");
     })
     .catch(err => {
       console.log("error:", err);
     });
 });
+
+
+
 
 module.exports = routes;
