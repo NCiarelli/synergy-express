ed663c812ad92eb8610ee6bc14b838eefeefe5d9
Ncia89 <nicholas.ciarelli@gmail.com>
2019-12-16 17:08:01 -0500

    Finished with functionality for local data version but with a database now.

--- routes.js
+++ routes.js
@@ -55,10 +55,16 @@ routes.post("/profile", (request, response) => {
 // GET /employees endpoint
 // Retrieve all employee data from the database
 routes.get("/employees", (req, res) => {
+  // DEBUG
+  console.log("GET /employees endpoint");
+  console.log("Request URL Parameters: ", req.params, "Request Body Data: ", req.body);
   // Make the SQL statement
-  const sql = "SELECT * FROM employees ORDER BY id";
+  const sql = "SELECT * FROM employees ORDER BY id;";
   // Send the SQL query
   pool.query(sql).then(result => {
+    // DEBUG
+    console.log("Database Response: ");
+    console.log(result.rows);
     // Send the result back
     res.json(result.rows);
   });
@@ -67,10 +73,16 @@ routes.get("/employees", (req, res) => {
 // GET /employees/:name endpoint
 // Retrieve employee data by name
 routes.get("/employees/:name", (req, res) => {
+  // DEBUG
+  console.log("GET /employees/:name endpoint");
+  console.log("Request URL Parameters: ", req.params, "Request Body Data: ", req.body);
   // Make the SQL statement
-  const sql = "SELECT * FROM employees WHERE name=$1::TEXT";
+  const sql = "SELECT * FROM employees WHERE name=$1::TEXT;";
   // Send the SQL query with the paramters
   pool.query(sql, [req.params.name]).then((result) => {
+    // DEBUG
+    console.log("Database Response: ");
+    console.log(result.rows);
     // Send the result back
     res.json(result.rows);
   });
@@ -79,10 +91,16 @@ routes.get("/employees/:name", (req, res) => {
 // POST /employees endpoint
 // Add employee to database with the name of the employee in the body of the request
 routes.post("/employees", (req, res) => {
+  // DEBUG
+  console.log("POST /employees endpoint");
+  console.log("Request URL Parameters: ", req.params, "Request Body Data: ", req.body);
   // Make the SQL statement
-  const sql = "INSERT INTO employees (name, dominant_personality) VALUES ($1::TEXT, 'None') RETURNING *";
+  const sql = "INSERT INTO employees (name, dominant_personality) VALUES ($1::TEXT, 'None') RETURNING *;";
   // Send the SQL query with the paramters
   pool.query(sql, [req.body.name]).then((result) => {
+    // DEBUG
+    console.log("Database Response: ");
+    console.log(result.rows);
     // Send the copied back resulting database entry with a status code of 201 
     res.status(201);
     res.json(result.rows[0]);
@@ -95,15 +113,21 @@ routes.post("/employees", (req, res) => {
 //   pool.query("UPDATE employees SET name=$1::TEXT")
 // });
 
-// PUT /employees/:id/personality-profile
+// PUT /employees/:id/personality-profile endpoint
 // Update the personality profile and dominant personality of an employee
-routes.put("/employees/update-all/:id", (req, res) => {
+routes.put("/employees/:id/personality-profile", (req, res) => {
+  // DEBUG
+  console.log("PUT /employees/:id/personality-profile endpoint");
+  console.log("Request URL Parameters: ", req.params, "Request Body Data: ", req.body);
   // Make the SQL statement
-  const sql = "UPDATE employees SET personality_profile=$1::TEXT, dominant_personality=$2::TEXT WHERE id=$3::INT RETURNING *";
+  const sql = "UPDATE employees SET personality_profile=$1::TEXT, dominant_personality=$2::TEXT WHERE id=$3::INT RETURNING *;";
   // Setup the params to update the  new personality profile and dominant personality, with the targeted employee's id
   let params = [req.body.personalityProfile, req.body.dominantPersonality, req.params.id];
   // Send the SQL query with the paramters
   pool.query(sql, params).then((result) => {
+    // DEBUG
+    console.log("Database Response: ");
+    console.log(result.rows);
     // Send the copied back resulting database entry
     res.json(result.rows[0]);
   });
@@ -112,21 +136,51 @@ routes.put("/employees/update-all/:id", (req, res) => {
 
 // SURVEY ENTRIES ENDPOINTS
 
-// GET /survey-entries/:employee-id
+// GET /employees/:id/survey-entries endpoint
 // Get all survey entries associated with an employee by id
-
-// POST /survey-entries/:employee-id
-// Add a survey entry for an employee
-routes.post("/survey-entries/:employee-id", (req, res) => {
+routes.get("/employees/:id/survey-entries", (req, res) => {
+  // DEBUG
+  console.log("GET /employees/:id/survey-entries endpoint");
+  console.log("Request URL Parameters: ", req.params, "Request Body Data: ", req.body);
   // Make the SQL statement
-  const sql = "INSERT INTO survey_entries (employee_id, content, created) VALUES ($1::TEXT, $2::TEXT, $::INT) RETURNING *";
+  const sql = "SELECT * FROM survey_entries WHERE employee_id=$1 ORDER BY id;";
+  // Setup the params to find the entries by the employee id
+  let params = [req.params.id];
+  // Send the SQL query
+  pool.query(sql, params).then(result => {
+    // DEBUG
+    console.log("Database Response: ");
+    console.log(result.rows);
+    // Send the result back
+    res.json(result.rows);
+  });
+});
+
+// POST /employees/:id/survey-entries endpoint
+// Add a survey entry for an employee
+routes.post("/employees/:id/survey-entries", (req, res) => {
+  // DEBUG
+  console.log("POST /employees/:id/survey-entries endpoint");
+  console.log("Request URL Parameters: ", req.params, "Request Body Data: ", req.body);
+  // Make the SQL statement
+  const sql = "INSERT INTO survey_entries (employee_id, content, created) VALUES ($1::INT, $2::TEXT, $3::BIGINT) RETURNING *;";
   // Setup the params to add the new survey entry
-  let params = [req.body.personalityProfile, req.body.dominantPersonality, req.params.id];
+  let params = [req.params.id, req.body.content, req.body.created];
+  // DEBUG
+  console.log("SQL Statement Parameters: ", params);
   // Send the SQL query with the paramters
   pool.query(sql, params).then((result) => {
+    // DEBUG
+    console.log("Database Response: ");
+    console.log(result.rows);
     // Send the copied back resulting database entry with a status code of 201 
     res.status(201);
     res.json(result.rows[0]);
+  }, (err) => {
+    console.log("Database Query Error: ");
+    console.log(err);
   });
-})
+});
+
+
 module.exports = routes;
